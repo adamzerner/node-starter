c6c8e4f378ce1be5b4ae3ea01c13f362360e7dd7
Adam Zerner <adamzerner@protonmail.com>
2020-06-25 19:57:36 -0700

    Delete account

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -6,5 +6,6 @@ router.post("/sign-in", require("./sign-in"));
 router.delete("/sign-out", require("./sign-out"));
 router.get("/me", require("./me"));
 router.get("/default", require("./default"));
+router.delete("/", require("./delete-account"));
 
 module.exports = router;
