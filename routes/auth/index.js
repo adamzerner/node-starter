2cdcc57db6b936b09643def99e997b081577c8a2
Adam Zerner <adamzerner@protonmail.com>
2020-06-25 22:26:52 -0700

    Change password

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -7,5 +7,6 @@ router.delete("/sign-out", require("./sign-out"));
 router.get("/me", require("./me"));
 router.get("/default", require("./default"));
 router.delete("/", require("./delete-account"));
+router.patch("/change-password", require("./change-password"));
 
 module.exports = router;
