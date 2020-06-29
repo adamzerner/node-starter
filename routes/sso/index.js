90e68065e152aabf56816bdd514e186fb950b56e
Adam Zerner <adamzerner@protonmail.com>
2020-06-28 17:40:30 -0700

    Stay signed in for SSO

--- routes/sso/index.js
+++ routes/sso/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const setMaxAgeOfCookie = (req, res, next) => {
+  req.session.cookie.maxAge = 1000 * 60 * 60 * 24 * 30; // 30 days
+  next();
+};
 
-router.use("/google", require("./google"));
-router.use("/twitter", require("./twitter"));
-router.use("/linkedin", require("./linkedin"));
+router.use("/google", setMaxAgeOfCookie, require("./google"));
+router.use("/twitter", setMaxAgeOfCookie, require("./twitter"));
+router.use("/linkedin", setMaxAgeOfCookie, require("./linkedin"));
 
 module.exports = router;
